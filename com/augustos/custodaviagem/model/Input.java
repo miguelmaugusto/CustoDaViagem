96745c9ab7796a88a43a614973d925a62681c04c
Miguel Augusto <miguel.augusto@gls-portugal.com>
2022-07-14 18:18:15 +0100

    Initial commit. Closes #1

---
+++ com/augustos/custodaviagem/model/Input.java
@@ -0,0 +1,12 @@
+package com.augustos.custodaviagem.model;
+
+public class Input {
+    /*
+     - Pedir ao utilizador que insira o titulo da viagem
+     - Pedir ao utilizador que insira o nr de kilometros
+     - Pedir ao utilizador que insira o consumo do veículo em litros/100km;
+     - Pedir ao utilizador que insira o preço do combustível usado em euros por litro;
+     - No final, apresentar ao utilizador o resultado do cálculo do custo total do combustível para a viagem.
+     */
+
+}
