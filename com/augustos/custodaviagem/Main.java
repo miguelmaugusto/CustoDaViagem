dd102d5444784d495d2767ccd55343a62aadc041
Miguel Augusto <miguel.augusto@gls-portugal.com>
2022-07-14 21:17:21 +0100

    Bonus 1. Closes #6

--- com/augustos/custodaviagem/Main.java
+++ com/augustos/custodaviagem/Main.java
@@ -17,30 +17,51 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         /** Program start **/
-        System.out.println("Insira o titulo da viagem");
-        String titulo = in.nextLine();
+        while(true) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("\t(1) Veiculo a combustao;");
+            System.out.println("\t(2) Veiculo eletrico;");
+            System.out.println("\t(3) Sair;");
+            String opcao = in.nextLine();
 
-        System.out.println("Insira o nr de kilometros");
-        Double kilometros = in.nextDouble();
+            if (!opcao.equalsIgnoreCase("3")) {
 
-        System.out.println("Insira o consumo do veículo em litros/100km");
-        Double consumo = in.nextDouble();
+                System.out.println("Insira o titulo da viagem");
+                String titulo = in.nextLine();
 
-        System.out.println("Insira o preço do combustível usado em euros por litro");
-        Float preco = in.nextFloat();
+                System.out.println("Insira o nr de kilometros");
+                Double kilometros = in.nextDouble();
 
-        System.out.println("Insira o preço total das portagens");
-        Float portagens = in.nextFloat();
+                boolean isICE = opcao.equalsIgnoreCase("1");
+                String tipoConsumo = isICE ? "litros/100km" : "kW/100km";
+                String tipoPreco = isICE ? "do combustivel usado em euros por litro" : "da eletricidade usado em euros por kW";
 
-        Double totalLitros = kilometros*consumo/100;
-        Double total = totalLitros*preco;
-        total+=portagens;
+                System.out.println("Insira o consumo do veículo em " + tipoConsumo);
+                Double consumo = in.nextDouble();
 
-        System.out.println("=======================================================");
-        System.out.println("Custo total da viagem: " + String.format("%.02f", total) + " euros.");
+                System.out.println("Insira o preço " + tipoPreco);
+                Float preco = in.nextFloat();
 
-        in.nextLine();
-        in.close();
+                System.out.println("Insira o preço total das portagens");
+                Float portagens = in.nextFloat();
+
+                Double totalLitros = (kilometros * consumo) / 100;
+                Double total = totalLitros * preco;
+                total += portagens;
+
+                System.out.println("=======================================================");
+                System.out.println("Custo total da viagem '" + titulo + "': " + String.format("%.02f", total) + " euros.");
+
+            } else if (opcao.equalsIgnoreCase("3")) {
+                return;
+            } else {
+                System.out.println("Opção Inválida");
+            }
+
+            in.nextLine();
+            in.nextLine();
+        }
+//            in.close();
 
     }
 }
